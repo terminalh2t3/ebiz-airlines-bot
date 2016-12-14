8affc829abb3efb46a468c16c76aa5f9f936f01f
Vu Tuan Anh <anhvt39@gmail.com>
2016-12-14 16:31:29 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+'use strict';
+const BootBot = require('./lib/BootBot');
+const config = require('config');
+const echoModule = require('./modules/echo');
+
+// App Secret can be retrieved from the App Dashboard
+const APP_SECRET = (process.env.MESSENGER_APP_SECRET) ?
+    process.env.MESSENGER_APP_SECRET :
+    config.get('app_secret');
+
+// Arbitrary value used to validate a webhook
+const VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN) ?
+    (process.env.MESSENGER_VALIDATION_TOKEN) :
+    config.get('verify_token');
+
+// Generate a page access token for your page from the App Dashboard
+const PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN) ?
+    (process.env.MESSENGER_PAGE_ACCESS_TOKEN) :
+    config.get('access_token');
+
+if (!(APP_SECRET && VALIDATION_TOKEN && PAGE_ACCESS_TOKEN)) {
+    console.error("Missing config values");
+    process.exit(1);
+}
+
+const bot = new BootBot({
+    accessToken : PAGE_ACCESS_TOKEN,
+    verifyToken : VALIDATION_TOKEN,
+    appSecret   : APP_SECRET
+});
+
+bot.module(echoModule);
+
+bot.hear('hello', (payload, chat) => {
+    chat.getUserProfile().then((user) => {
+        chat.say(`Hello, ${user.first_name}!`);
+    });
+});
+
+bot.start();
