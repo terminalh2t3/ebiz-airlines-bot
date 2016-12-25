39919e42bee853a7b619198735d93c80fd84ef09
Vu Tuan Anh <anhvt39@gmail.com>
2016-12-25 17:31:24 +0700

    add find flights

--- http/controllers/Test.js
+++ http/controllers/Test.js
@@ -1,15 +1,14 @@
 "use strict";
 const   BaseController = require("./Base"),
     View = require("../views/Base");
+const FlightSchedule = require('../../lib/api/business/FlightScheduleBusiness');
 
 module.exports = BaseController.extend({
     name: "Test",
     content: null,
     flightSchedule: function(req, res, next) {
-        const FlightSchedule = require('../../lib/api/business/FlightScheduleBusiness');
-        FlightSchedule.findFlight("100000", "700000", "2016-12-26", null, function(data){
-            const v = new View(res, 'test');
-            v.render({content: data});
+        FlightSchedule.findFlight("HAN", "SGN", "2016-12-26", function(error, data){
+            res.json(data);
         });
     }
 });
