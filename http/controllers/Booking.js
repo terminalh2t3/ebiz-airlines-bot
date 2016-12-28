fda5c1c2f2047103d51e49e1c28dd4db526c6fba
Hoang Nguyen <hoangnn1@asiantech.vn>
2016-12-29 00:57:33 +0700

    Add Check In page

--- http/controllers/Booking.js
+++ http/controllers/Booking.js
@@ -8,29 +8,37 @@ module.exports = BaseController.extend({
     name: "Home",
     content: null,
     checkinPage: function (req, res, next) {
-        const passengerId = req.query.passenger_id;
-        const bookingId   = req.query.booking_id;
+        const passengerId = req.query.passenger;
+        const bookingId   = req.query.booking;
         const datetime = require('node-datetime');
         const BookingBusiness = require('../../lib/api/business/BookingBusiness');
         BookingBusiness.getCheckInDetail(passengerId, bookingId, function (error, model) {
-            res.render('flight/checkin', {
-                booking: model,
-                datetime: datetime
-            });
+            if(model) {
+                res.render('flight/checkin', {
+                    booking: model,
+                    datetime: datetime
+                });
+            } else {
+                res.render('flight/checkin_success');
+            }
         });
     },
     updateCheckin: function (req, res, next) {
-        const passengerId = req.query.passenger_id;
-        const bookingId   = req.query.booking_id;
+        const passengerId = req.body.passenger_id;
+        const bookingId   = req.body.booking_id;
+        const flightId    = req.body.flight_id;
         const BookingBusiness = require('../../lib/api/business/BookingBusiness');
+        const template = require('../../lib/bot/utils/airport-template');
         BookingBusiness.updateCheckInStatus(passengerId, bookingId, function (error, model) {
             if(model) {
                 console.log("Update checkin status for passenger" + passengerId);
+                //Send Boarding Pass
+                template.boardingPassOnePassenger(flightId, bookingId);
                 res.redirect('/checkin/success');
             }
         });
     },
     checkinSuccess: function (req, res, next) {
-        res.render('close');
+        res.render('flight/checkin_success');
     }
 });
