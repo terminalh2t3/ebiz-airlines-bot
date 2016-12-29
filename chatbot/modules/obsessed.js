18d2f432cd66a808f0b4dff19fd8dff73c165b18
Vu Tuan Anh <anhvt39@gmail.com>
2016-12-29 22:09:08 +0700

    add handle obsessed

---
+++ chatbot/modules/obsessed.js
@@ -0,0 +1,11 @@
+'use strict';
+module.exports = (bot) => {
+    bot.hear(['fuck you', 'fuck', 'shit', 'stupid', /hey( there)?/i], (payload, chat) => {
+        // Send a text message followed by another text message that contains a typing indicator
+        chat.say('You may be banned if continue talking with me like that');
+    });
+
+    bot.hear(['sorry', 'I\'m sorry]'], (payload, chat) => {
+        chat.say('OK! Don\'t worry about that.');
+    });
+};
