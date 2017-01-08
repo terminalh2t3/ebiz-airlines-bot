26fbd0df3a9d8db32a08943fec9b194c402fe1fe
Hoang Nguyen <hoangnn1@asiantech.vn>
2017-01-08 17:07:11 +0700

    Update Check In reminder , checkin page, send boarding pass to passenger

--- http/controllers/Booking.js
+++ http/controllers/Booking.js
@@ -8,12 +8,12 @@ module.exports = BaseController.extend({
     name: "Home",
     content: null,
     checkinPage: function (req, res, next) {
-        const passengerId = req.query.passenger;
-        const bookingId   = req.query.booking;
+        const passengerSfId = req.query.passenger;
+        const bookingSfId   = req.query.booking;
         const datetime = require('node-datetime');
         const BookingBusiness = require('../../lib/api/business/BookingBusiness');
         const url = require('url');
-        BookingBusiness.getCheckInDetail(passengerId, bookingId, function (error, model) {
+        BookingBusiness.getCheckInDetail(bookingSfId, passengerSfId, function (error, model) {
             if(model) {
                 res.render('flight/checkin', {
                     booking: model,
@@ -26,17 +26,18 @@ module.exports = BaseController.extend({
         });
     },
     updateCheckin: function (req, res, next) {
-        const passengerId = req.body.passenger_id;
-        const bookingId   = req.body.booking_id;
-        const flightId    = req.body.flight_id;
+        const passengerSfid = req.body.passenger_sfid;
+        const flightSfid    = req.body.flight_sfid;
+        const bookingId     = req.body.booking_id;
+        const bookingSfid   = req.body.booking_sfid;
         const BookingBusiness = require('../../lib/api/business/BookingBusiness');
         const template = require('../../lib/bot/utils/airport-template');
-        BookingBusiness.updateCheckInStatus(passengerId, bookingId, function (error, model) {
+        BookingBusiness.updateCheckInStatus(bookingId, function (error, model) {
             if(model) {
                 //Send Boarding Pass
                 setTimeout(function () {
-                    console.log("Update checkin status for passenger" + passengerId);
-                    template.boardingPassOnePassenger(flightId, bookingId);
+                    console.log("Update checkin status for passenger" + passengerSfid);
+                    template.boardingPassOnePassenger(flightSfid, bookingSfid);
                 }, 10000);
                 res.send(true);
             }
